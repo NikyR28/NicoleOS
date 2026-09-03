47191c5f054a83f730c6b5dbff97b4ac0405c975
Giacomo Gorini <giacomo.gorini.dev@gmail.com>
2026-09-03 15:01:02 +0200

    Caricamento Vercel NikyR28

--- frontend/src/components/BookGrid.jsx
+++ frontend/src/components/BookGrid.jsx
@@ -1,26 +1,34 @@
-async function BookGrid(){
-    return(
-        <main>
-            <div>
-                {books.map((book)=>{
-                    <CardBook
-                        key={book.id}
-                        title={book.title}
-                        author={book.author}
-                        pages={book.pages}
-                        genre={book.genre}
-                        coverUrl={book.coverUrl}
-                        status={book.status}
-                        startDate={book.startDate}
-                        startEnd={book.startEnd}
-                        rating={book.rating}
-                        reviewText={book.reviewText}
-                    //Status plus features  Biblioteca (Data di restituzione) / Posseduto può essere Kindle copertina rigida o flessibile
-                >
-                    </CardBook>
+import styles from './BookGrid.module.css'
+
+function BookGrid({ books }) {
+    if(books.length==0||!books) return( <div className={styles['no-books']}>Nessun Libro Trovato</div>);
+    else{
+        return (
+                <div>
+                    {books.map((book) => {
+                    return (
+                        <CardBook
+                            key={book.id}
+                            title={book.title}
+                            author={book.author}
+                            pages={book.pages}
+                            genre={book.genre}
+                            coverUrl={book.cover_url}
+                            status={book.status}
+                            startDate={book.start_date}
+                            startEnd={book.start_end}
+                            rating={book.rating}
+                            reviewText={book.review_text}
+                            format={book.format}
+                            binding={book.binding}
+                            isLibrary={book.is_library}
+                            returnDate={book.is_library ? book.return_date : null}
+                        >
+                        </CardBook>
+                    );
                 })}
-            </div>            
-        </main>
-    );
+            </div>
+        );
+    }
 }
 export default BookGrid;
